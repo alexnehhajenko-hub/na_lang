9a2181022a116addebf0b64cf2bd26635553ca35
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2026-01-14 10:55:25 +0100

    Update page.tsx

--- app/[locale]/quote/page.tsx
+++ app/[locale]/quote/page.tsx
@@ -1,11 +1,5 @@
 import QuoteCalculatorForm from "@/src/components/QuoteCalculatorForm";
 
 export default function QuotePage() {
-  return (
-    <div className="container">
-      <section className="section">
-        <QuoteCalculatorForm />
-      </section>
-    </div>
-  );
+  return <QuoteCalculatorForm />;
 }
