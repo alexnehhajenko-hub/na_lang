49bb76ddc48abee5c4b15bc32d0076504fb84eb3
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2026-01-14 10:24:40 +0100

    Add QuotePage component with QuoteCalculatorForm

---
+++ app/[locale]/quote/page.tsx
@@ -0,0 +1,11 @@
+import QuoteCalculatorForm from "@/src/components/QuoteCalculatorForm";
+
+export default function QuotePage() {
+  return (
+    <div className="container">
+      <section className="section">
+        <QuoteCalculatorForm />
+      </section>
+    </div>
+  );
+}
